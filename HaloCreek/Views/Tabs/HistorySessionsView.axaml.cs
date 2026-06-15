138ca07db75f813f197ae02968e552ada02c0605
cuikai <cuikai603@163.com>
2026-06-15 11:43:36 +0800

    双击自动resume

--- HaloCreek/Views/Tabs/HistorySessionsView.axaml.cs
+++ HaloCreek/Views/Tabs/HistorySessionsView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using HaloCreek.Models;
+using HaloCreek.ViewModels.Tabs;
 
 namespace HaloCreek.Views.Tabs
 {
@@ -7,6 +10,22 @@ namespace HaloCreek.Views.Tabs
         public HistorySessionsView()
         {
             InitializeComponent();
+        }
+
+        private void HistorySession_OnDoubleTapped(object? sender, TappedEventArgs e)
+        {
+            if (DataContext is not HistorySessionsViewModel viewModel
+                || sender is not Control { DataContext: HistorySessionInfo session })
+            {
+                return;
+            }
+
+            viewModel.SelectedSession = session;
+            if (viewModel.ResumeCommand.CanExecute(session))
+            {
+                viewModel.ResumeCommand.Execute(session);
+                e.Handled = true;
+            }
         }
     }
 }
