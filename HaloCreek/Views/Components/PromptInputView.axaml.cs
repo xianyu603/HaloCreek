7ed434f3c34807a73e8715af9e77e0947c3c80c7
cuikai <cuikai603@163.com>
2026-06-18 18:40:42 +0800

    回退悬浮的尝试 实际无法收敛

--- HaloCreek/Views/Components/PromptInputView.axaml.cs
+++ HaloCreek/Views/Components/PromptInputView.axaml.cs
@@ -1,24 +1,15 @@
-using System;
-using System.ComponentModel;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Presenters;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.VisualTree;
 using HaloCreek.ViewModels.Components;
 
 namespace HaloCreek.Views.Components
 {
     public partial class PromptInputView : UserControl
     {
-        private PromptInputViewModel? _subscribedViewModel;
-
         public PromptInputView()
         {
             InitializeComponent();
-            CompletionPopup.PlacementTarget = PromptTextBox;
-            DataContextChanged += PromptInputView_OnDataContextChanged;
             PromptTextBox.AddHandler(
                 InputElement.KeyDownEvent,
                 PromptTextBox_OnKeyDown,
@@ -38,54 +29,6 @@ namespace HaloCreek.Views.Components
             {
                 e.Handled = true;
             }
-        }
-
-        private void PromptInputView_OnDataContextChanged(object? sender, EventArgs e)
-        {
-            if (_subscribedViewModel is not null)
-            {
-                _subscribedViewModel.PropertyChanged -= ViewModel_OnPropertyChanged;
-            }
-
-            _subscribedViewModel = DataContext as PromptInputViewModel;
-            CompletionPopup.DataContext = _subscribedViewModel;
-            if (_subscribedViewModel is not null)
-            {
-                _subscribedViewModel.PropertyChanged += ViewModel_OnPropertyChanged;
-            }
-        }
-
-        private void ViewModel_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (sender is not PromptInputViewModel { IsCompletionOpen: true })
-            {
-                return;
-            }
-
-            if (e.PropertyName == nameof(PromptInputViewModel.IsCompletionOpen)
-                || e.PropertyName == nameof(PromptInputViewModel.PromptText)
-                || e.PropertyName == nameof(PromptInputViewModel.PromptCaretIndex))
-            {
-                CompletionPopup.PlacementRect = GetCompletionPlacementRect();
-            }
-        }
-
-        private Rect GetCompletionPlacementRect()
-        {
-            var presenter = PromptTextBox.FindDescendantOfType<TextPresenter>();
-            if (presenter is null)
-            {
-                return new Rect(PromptTextBox.Padding.Left, PromptTextBox.Padding.Top, 0, PromptTextBox.FontSize);
-            }
-
-            var textLength = PromptTextBox.Text?.Length ?? 0;
-            var caretIndex = Math.Clamp(PromptTextBox.CaretIndex, 0, textLength);
-            var caretBounds = presenter.TextLayout.HitTestTextPosition(caretIndex);
-            var caretTopLeft = presenter.TranslatePoint(new Point(caretBounds.X, caretBounds.Y), PromptTextBox);
-
-            return caretTopLeft is { } point
-                ? new Rect(point, caretBounds.Size)
-                : new Rect(PromptTextBox.Padding.Left, PromptTextBox.Padding.Top, 0, PromptTextBox.FontSize);
         }
     }
 }
